9122084eb73e104c651701f92da539641f2611be
Daniel Zhuo <sirdanieldathird@gmail.com>
2024-11-22 21:47:44 -0500

    Update website for assignment 2

--- src/components/assignments/Assignment2.tsx
+++ src/components/assignments/Assignment2.tsx
@@ -6,9 +6,9 @@ const Assignment2: React.FC = () => (
     <AssignmentPage
         title="Assignment 2"
         folder="a2"
-        reportFilename="A2-Report.pdf" // Replace with actual filename
-        presentationFilename="A2-Presentation.pdf" // Replace with actual filename
-        videoFilename="A2-Video.mp4" // Replace with actual filename
+        reportFilename="A2-Concrete_Architecture_Report.pdf" // Replace with actual filename
+        presentationFilename="A2-Concrete_Architecture_Presentation.pdf" // Replace with actual filename
+        videoFilename="A2-VideoPresentation.mp4" // Replace with actual filename
     />
 );
 
